5ffb367a5ebff2b7d1a54bb26a0a4dbed2476ec0
thanhminhspk05 <69253365+thanhminhspk05@users.noreply.github.com>
2023-10-10 10:43:43 +0700

    save

--- app/components/Header.tsx
+++ app/components/Header.tsx
@@ -1,18 +1,27 @@
+"use client";
+
+import Link from "next/link";
 import { FC, useState } from "react";
+import { HiOutlineMenuAlt3 } from "react-icons/hi";
+
+import NavItems from "../utils/NavItems";
+import { ThemeSwitcher } from "../utils/ThemeSwitcher";
 
 type Props = {
   open: boolean;
   setOpen: (open: boolean) => void;
   activeItem: number;
+  //   route: string;
+  //   setRoute: (route: string) => void;
 };
 
-const Header: FC<Props> = (props) => {
+const Header: FC<Props> = ({ activeItem, setOpen }) => {
   const [active, setActive] = useState(false);
   const [openSidebar, setOpenSidebar] = useState(false);
 
   if (typeof window !== "undefined") {
     window.addEventListener("scroll", () => {
-      if (window.scrollY > 80) {
+      if (window.scrollY > 85) {
         setActive(true);
       } else {
         setActive(false);
@@ -20,18 +29,119 @@ const Header: FC<Props> = (props) => {
     });
   }
 
+  const handleClose = (e: any) => {
+    if (e.target.id === "screen") {
+      {
+        setOpenSidebar(false);
+      }
+    }
+  };
+
   return (
-    <div className="w-full relative">
-      <div
-        className={`${
-          active
-            ? "dark:bg-opacity-50 bg-white dark:bg-gradient-to-b dark:from-gray-900 dark:to-black fixed top-0 left-0 w-full h-[80px] z-[80] border-b dark:border-[#ffffff1c] shadow-xl transition duration-500"
-            : "w-full border-b dark:border-[#ffffff1c] h-[80px] z-[80] dark:shadow"
-        }`}
-      >
-        Hello
+    <>
+      <div className="w-full relative">
+        <div
+          className={`${
+            active
+              ? "dark:bg-opacity-50 bg-white dark:bg-gradient-to-b dark:from-gray-900 dark:to-black fixed top-0 left-0 w-full h-[80px] z-[80] border-b dark:border-[#ffffff1c] shadow-xl transition duration-500"
+              : "w-full border-b dark:border-[#ffffff1c] h-[80px] z-[80] dark:shadow"
+          }`}
+        >
+          <div className="w-[95%] 800px:w-[92%] m-auto py-2 h-full">
+            <div className="w-full h-[80px] flex items-center justify-between p-3">
+              <div>
+                <Link
+                  href={"/"}
+                  className={`text-[25px] font-Poppins font-[500] text-black dark:text-white`}
+                >
+                  ELearning
+                </Link>
+              </div>
+              <div className="flex items-center">
+                <NavItems activeItem={activeItem} isMobile={false} />
+                <ThemeSwitcher />
+                {/* only for mobile */}
+                <div className="800px:hidden">
+                  <HiOutlineMenuAlt3
+                    size={25}
+                    className="cursor-pointer dark:text-white text-black"
+                    onClick={() => setOpenSidebar(true)}
+                  />
+                </div>
+                {/* {userData ? (
+                    <Link href={"/profile"}>
+                      <Image
+                        src={
+                          userData?.user.avatar
+                            ? userData.user.avatar.url
+                            : avatar
+                        }
+                        alt=""
+                        width={30}
+                        height={30}
+                        className="w-[30px] h-[30px] rounded-full cursor-pointer"
+                        style={{
+                          border:
+                            activeItem === 5 ? "2px solid #37a39a" : "none",
+                        }}
+                      />
+                    </Link>
+                  ) : (
+                    <HiOutlineUserCircle
+                      size={25}
+                      className="hidden 800px:block cursor-pointer dark:text-white text-black"
+                      onClick={() => setOpen(true)}
+                    />
+                  )} */}
+              </div>
+            </div>
+          </div>
+
+          {/* mobile sidebar */}
+          {openSidebar && (
+            <div
+              className="fixed w-full h-screen top-0 left-0 z-[99999] dark:bg-[unset] bg-[#00000024]"
+              onClick={handleClose}
+              id="screen"
+            >
+              <div className="w-[70%] fixed z-[999999999] h-screen bg-white dark:bg-slate-900 dark:bg-opacity-90 top-0 right-0">
+                <NavItems activeItem={activeItem} isMobile={true} />
+                {/* {userData?.user ? (
+                    <Link href={"/profile"}>
+                      <Image
+                        src={
+                          userData?.user.avatar
+                            ? userData.user.avatar.url
+                            : avatar
+                        }
+                        alt=""
+                        width={30}
+                        height={30}
+                        className="w-[30px] h-[30px] rounded-full ml-[20px] cursor-pointer"
+                        style={{
+                          border:
+                            activeItem === 5 ? "2px solid #37a39a" : "none",
+                        }}
+                      />
+                    </Link>
+                  ) : (
+                    <HiOutlineUserCircle
+                      size={25}
+                      className="hidden 800px:block cursor-pointer dark:text-white text-black"
+                      onClick={() => setOpen(true)}
+                    />
+                  )} */}
+                <br />
+                <br />
+                <p className="text-[16px] px-2 pl-5 text-black dark:text-white">
+                  Copyright © 2023 ELearning
+                </p>
+              </div>
+            </div>
+          )}
+        </div>
       </div>
-    </div>
+    </>
   );
 };
 
