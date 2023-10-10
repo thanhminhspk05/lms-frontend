15ebe40bc6c2e7479b4ab373ab82e8822dd9524e
thanhminhspk05 <69253365+thanhminhspk05@users.noreply.github.com>
2023-10-10 10:11:15 +0700

    save

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,14 @@
 "use client";
-import { FC } from "react";
+import { FC, useState } from "react";
+import Header from "./components/Header";
 import Heading from "./utils/Heading";
 
 interface Props {}
 
 const Page: FC<Props> = (props) => {
+  const [open, setOpen] = useState(false);
+  const [activeItem, setActiveItem] = useState(0);
+
   return (
     <div>
       <Heading
@@ -12,6 +16,7 @@ const Page: FC<Props> = (props) => {
         description="ELearning is a platform for students to learn and get help from teachers"
         keywords="Prograaming,MERN,Redux,Machine Learning"
       />
+      <Header open={open} setOpen={setOpen} activeItem={activeItem} />
     </div>
   );
 };
--- app/utils/Heading.tsx
+++ app/utils/Heading.tsx
@@ -11,7 +11,6 @@ const Heading: FC<HeadProps> = ({ title, description, keywords }) => {
   return (
     <>
       <title>{title}</title>
-
       <meta name="viewport" content="width=device-width, initial-scale=1" />
       <meta name="description" content={description} />
       <meta name="keywords" content={keywords} />
